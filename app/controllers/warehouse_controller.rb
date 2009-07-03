1bf54aa68ec6b80c1fa4ea23a01fc20d75864590
Justin Rocha <xenith@xenith.org>
2009-07-03 16:38:14 -0700

    First work on the stock audit function.

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -1,5 +1,12 @@
 class WarehouseController < ApplicationController
+  around_filter :shopify_session
+  
   def receive_shipment
   end
 
+  def stock_audit
+    #@vendor_names = ShopifyAPI::Product.find(:all, :group => vendor, :order => 'vendor').map{|product| product.vendor}.uniq
+    @products = ShopifyAPI::Product.find(:all, :group => :vendor, :order => 'vendor')
+    orders = ShopifyAPI::Order.find(:all, :limit => 'financial_status = pending')
+  end
 end
