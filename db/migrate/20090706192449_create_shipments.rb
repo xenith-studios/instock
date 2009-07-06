f4494a947d3b2e229f44fc564b201c5337f54ada
Daniel Hirsmuller-Counts <danielhc@yt.local>
2009-07-06 12:25:25 -0700

    typo fix

---
+++ db/migrate/20090706192449_create_shipments.rb
@@ -0,0 +1,15 @@
+class CreateShipments < ActiveRecord::Migration
+  def self.up
+    create_table :shipments do |t|
+      t.integer :shopify_store_id
+      t.datetime :created_at
+      t.datetime :update_at
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :shipments
+  end
+end
