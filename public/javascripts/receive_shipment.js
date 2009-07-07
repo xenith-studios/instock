f8ea32c97af1b499ea7c484daf68d7e4eadefa50
Daniel Hirsmuller-Counts <danielhc@yt.local>
2009-07-07 14:43:58 -0700

    Updated the javascript to update on click instead of on change.

--- public/javascripts/receive_shipment.js
+++ public/javascripts/receive_shipment.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#vendors').change(function() {
+	$('#vendors').click(function() {
 		var vendor = $(this).val();
 		$('#productVariants').empty();
 		$('#productVariants').load("/warehouse/vendor_products", {vendor_name: vendor});
