1cee7816995a53e3491dd5f5455370be90b62e36
Daniel Hirsmuller-Counts <danielhc@yt.local>
2009-07-13 14:59:29 -0700

    Modifying shipment items so that they update the shopify stock counts.

--- app/models/shipment_item.rb
+++ app/models/shipment_item.rb
@@ -1,3 +1,14 @@
 class ShipmentItem < ActiveRecord::Base
   belongs_to :shipment
+  
+  before_create do |si|
+    vid = si.variant_id
+    pid = si.product_id
+    variant = ShopifyAPI::Variant.find(vid, :params => { :product_id => pid })
+    variant.inventory_quantity += si.count
+    unless(variant.save)
+      si.errors.add_to_base("Could not update the shoppify store!")
+    end
+  end
+  
 end
