eb768406007c26430ec5f8ecacfd214894d2ca31
Daniel Hirsmuller-Counts <danielhc@yt.local>
2009-07-13 11:11:04 -0700

    typo fixed

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -9,60 +9,60 @@ class ShipmentsController < ApplicationController
   end
 
   def show
-    @shipmnet = Shipment.find(params[:id])
+    @shipment = Shipment.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @shipmnet }
+      format.xml  { render :xml => @shipment }
     end
   end
 
   def new
-    @shipmnet = Shipment.new
+    @shipment = Shipment.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @shipmnet }
+      format.xml  { render :xml => @shipment }
     end
   end
 
   def edit
-    @shipmnet = Shipment.find(params[:id])
+    @shipment = Shipment.find(params[:id])
   end
 
   def create
-    @shipmnet = Shipment.new(params[:shipmnet])
+    @shipment = Shipment.new(params[:shipment])
 
     respond_to do |format|
-      if @shipmnet.save
+      if @shipment.save
         flash[:notice] = 'Shipment was successfully created.'
-        format.html { redirect_to(@shipmnet) }
-        format.xml  { render :xml => @shipmnet, :status => :created, :location => @shipmnet }
+        format.html { redirect_to(@shipment) }
+        format.xml  { render :xml => @shipment, :status => :created, :location => @shipment }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @shipmnet.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @shipment.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   def update
-    @shipmnet = Shipment.find(params[:id])
+    @shipment = Shipment.find(params[:id])
 
     respond_to do |format|
-      if @shipmnet.update_attributes(params[:shipmnet])
+      if @shipment.update_attributes(params[:shipment])
         flash[:notice] = 'Shipment was successfully updated.'
-        format.html { redirect_to(@shipmnet) }
+        format.html { redirect_to(@shipment) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @shipmnet.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @shipment.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    @shipmnet = Shipment.find(params[:id])
-    @shipmnet.destroy
+    @shipment = Shipment.find(params[:id])
+    @.destroy
 
     respond_to do |format|
       format.html { redirect_to(admin_shipment_url) }
