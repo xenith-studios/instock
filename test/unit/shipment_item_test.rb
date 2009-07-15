94ff06b5227e83ead1c6de7b2e692e20b33c9d8d
Daniel Hirsmuller-Counts <danielhc@yt.local>
2009-07-15 15:22:05 -0700

    Updated tests.

--- test/unit/shipment_item_test.rb
+++ test/unit/shipment_item_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class ShipmentItemTest < ActiveSupport::TestCase
+  context "A ShipmetItem" do
+    setup do
+      @invalid = ShipmentItem.new()
+      @invalid.count = -20
+    end
+    should "not allow a negative :count" do
+      assert_not_valid(@invalid)
+    end
+  end #context
 end
--- test/unit/shipment_test.rb
+++ test/unit/shipment_test.rb
@@ -6,7 +6,12 @@ class ShipmentTest < ActiveSupport::TestCase
       @shipment = Shipment.new()
     end #setup
     
-    should_validate_presence_of(:created_by, :shopify_store_id)
+    should "require :shopify_store_id and :created_by" do
+      assert_invalid(@shipment)
+      @shipment.shopify_store_id = 123654
+      @shipment.created_by = "Bob"
+      assert_valid(@shipment)
+    end
     
   end #context
 end
