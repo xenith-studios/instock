4ca1b3fa5cfdbaba5b76752e481ba9fc3cc0d835
Justin Rocha <xenith@xenith.org>
2009-07-16 18:03:47 -0700

    New way of doing things. Most of the data is now built. Time to make the view use it.

--- app/controllers/stock_audits_controller.rb
+++ app/controllers/stock_audits_controller.rb
@@ -13,40 +13,33 @@ class StockAuditsController < ApplicationController
   end
   
   def new
+    # Set up the needed variables
     @audit = StockAudit.new
     @audit.stock_audit_items.build
     build_products_and_orders
-
-    # Set up the needed hashes
-    products = {}
-    variants = {}
     fulfillments = {}
     
     # Gather the data from Shopify
-    all_products = ShopifyAPI::Product.find(:all, :sort => :title)
+    products = ShopifyAPI::Product.find(:all, :sort => :title)
     orders = ShopifyAPI::Order.find(:all, :params => { :status => "open", :fulfillment_status => "unshipped", :fulfillment_status => "partial"})
-    
-    # Build the hashes needed by the actions
-    vendors = all_products.map{|product| product.vendor}.uniq.sort{|a,b| a.casecmp(b)}
-    vendors.each do |vendor|
-      products[vendor] = all_products.select{|p| p.vendor == vendor}
-    end
-    all_products.each do |product|
-      variants[product] = product.variants.sort{|a,b| a.title.casecmp(b.title)}
-    end
     orders.each do |order|
       fulfillments[order] = ShopifyAPI::Fulfillment.find(:all, :params => { :order_id => order.id } )
     end
 
-    variants.each do |variant|
-      item = StockAuditItem.new
-      item.product_id = variant.product_id
-      item.variant_id = variant.object_id
-      item.title = variant.title
-      item.sku = variant.sku
-      item.shopify_count = variant.inventory_quantity
-      item.pending_count = 0
-      item.expected_count = item.shopify_count + item.pending_count
+    # Build all the StockAuditItems with data and insert them into @audit
+    products.each do |product|
+      product.variants.each do |variant|
+        item = StockAuditItem.new
+        item.vendor = product.vendor
+        item.product_id = product.object_id
+        item.variant_id = variant.object_id
+        item.title = variant.title
+        item.sku = variant.sku
+        item.shopify_count = variant.inventory_quantity
+        item.pending_count = orders.collect{|order| order.line_items.select{|i| i.variant_id == variant.object_id}.map{|i| i.quantity }}.flatten.inject{|sum,element| sum + element }
+        item.expected_count = item.shopify_count + (item.pending_count ? item.pending_count : 0)
+        #@audit.stock_audit_items << item
+      end
     end
     
     respond_to do |format|
--- app/models/stock_audit_item.rb
+++ app/models/stock_audit_item.rb
@@ -1,5 +1,6 @@
 class StockAuditItem < ActiveRecord::Base
   belongs_to :stock_audit
   
+  attr_accessor :shopify_count, :pending_count, :vendor
   validates_presence_of :product_id, :variant_id, :title, :sku, :expected_count, :actual_count
 end
