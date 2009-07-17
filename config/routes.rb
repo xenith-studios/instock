2331d6e4483d5e8a0d4e5df05dd191a5d8c32b93
Justin Rocha <xenith@xenith.org>
2009-07-17 14:50:48 -0700

    Fix routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'home'
-  map.connect 'login/:action', :controller => 'login'
+  map.connect 'login/:action/:id', :controller => 'login'
+  map.connect 'warehouse/:action/:id', :controller => 'warehouse'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
