b2ca01ce31e3045879bc661f33d8b87ce0656e31
Justin Rocha <xenith@xenith.org>
2009-07-16 17:18:29 -0700

    Working on a big change.

--- app/controllers/stock_audits_controller.rb
+++ app/controllers/stock_audits_controller.rb
@@ -16,6 +16,39 @@ class StockAuditsController < ApplicationController
     @audit = StockAudit.new
     @audit.stock_audit_items.build
     build_products_and_orders
+
+    # Set up the needed hashes
+    products = {}
+    variants = {}
+    fulfillments = {}
+    
+    # Gather the data from Shopify
+    all_products = ShopifyAPI::Product.find(:all, :sort => :title)
+    orders = ShopifyAPI::Order.find(:all, :params => { :status => "open", :fulfillment_status => "unshipped", :fulfillment_status => "partial"})
+    
+    # Build the hashes needed by the actions
+    vendors = all_products.map{|product| product.vendor}.uniq.sort{|a,b| a.casecmp(b)}
+    vendors.each do |vendor|
+      products[vendor] = all_products.select{|p| p.vendor == vendor}
+    end
+    all_products.each do |product|
+      variants[product] = product.variants.sort{|a,b| a.title.casecmp(b.title)}
+    end
+    orders.each do |order|
+      fulfillments[order] = ShopifyAPI::Fulfillment.find(:all, :params => { :order_id => order.id } )
+    end
+
+    variants.each do |variant|
+      item = StockAuditItem.new
+      item.product_id = variant.product_id
+      item.variant_id = variant.object_id
+      item.title = variant.title
+      item.sku = variant.sku
+      item.shopify_count = variant.inventory_quantity
+      item.pending_count = 0
+      item.expected_count = item.shopify_count + item.pending_count
+    end
+    
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @audit }
@@ -78,9 +111,16 @@ class StockAuditsController < ApplicationController
 
 private
   def build_products_and_orders
+    # Set up the needed hashes
     @products = {}
     @variants = {}
+    fulfillments = {}
+    
+    # Gather the data from Shopify
     all_products = ShopifyAPI::Product.find(:all, :sort => :title)
+    @orders = ShopifyAPI::Order.find(:all, :params => { :status => "open", :fulfillment_status => "unshipped", :fulfillment_status => "partial"})
+    
+    # Build the hashes needed by the actions
     @vendors = all_products.map{|product| product.vendor}.uniq.sort{|a,b| a.casecmp(b)}
     @vendors.each do |vendor|
       @products[vendor] = all_products.select{|p| p.vendor == vendor}
@@ -88,8 +128,8 @@ private
     all_products.each do |product|
       @variants[product] = product.variants.sort{|a,b| a.title.casecmp(b.title)}
     end
-
-    orders = ShopifyAPI::Order.find(:all, :params => { :status => "open", :fulfilment_status => "unshipped", :fulfilment_status => "partial"})
-    
+    @orders.each do |order|
+      fulfillments[order] = ShopifyAPI::Fulfillment.find(:all, :params => { :order_id => order.id } )
+    end
   end
 end
