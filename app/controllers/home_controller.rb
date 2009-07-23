996479d9043666cea353f92119815b1af4d00acf
Daniel Hirsmuller-Counts <danielhc@yt.local>
2009-07-23 13:22:34 -0700

    Removed most of the generated stuff. Updated home/index to be a welcome page for inStock users.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,11 @@
 class HomeController < ApplicationController
   layout "application"
   around_filter :shopify_session, :except => 'welcome'
-  
-  def welcome
-    current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
-    @callback_url = "http://#{current_host}/login/finalize"
-  end
-  
+    
   def index
-    # get 3 products
-    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 3})
-
-    # get latest 3 orders
-    @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 3, :order => "created_at DESC" })
   end
   
+  def design
+  end
   
 end
