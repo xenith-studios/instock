49ac56bc1887505ef6e964789c2ccbef1518024f
Justin Rocha <justin@bazaarconsulting.com>
2009-08-03 16:54:09 -0700

    Put some information here.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,17 @@
 class HomeController < ApplicationController
   layout "application"
-  around_filter :shopify_session
+  around_filter :shopify_session, :except => [:about, :contact, :support]
   
   def index
   end
-  
-  def design
-  end
-  
+
   def contact
   end
   
   def about
-    
   end
   
   def support
-    
   end
   
   def preferences
