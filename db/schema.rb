76119e4ae92eff1836dcc38e5faf1b89f839fa3a
Daniel Hirsmuller-Counts <danielhc@yt.local>
2009-08-03 16:14:18 -0700

    What did I change?

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20090801002302) do
 
   create_table "receiving_items", :force => true do |t|
