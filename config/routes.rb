e72ae386aa0bb3c495458f45bab57216ce9ebb55
Justin Rocha <justin@bazaarconsulting.com>
2009-08-03 12:57:47 -0700

    Remap controllers a bit.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ ActionController::Routing::Routes.draw do |map|
   
   map.root :controller => 'home'
 
-  map.resources :stock_audits, :has_many => :stock_audit_items
+  map.resources :stock_audits, :has_many => :stock_audit_items, :as => "audits"
   map.resources :receivings, :has_many => :receiving_items
-  map.resources :stock_adjustments, :has_many => :stock_adjustment_items
+  map.resources :stock_adjustments, :has_many => :stock_adjustment_items, :as => "adjustments"
 
   map.connect 'login/:action/:id', :controller => 'login'
 
