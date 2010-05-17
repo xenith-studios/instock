7c63ce73c3e3df1ec5f2bda01811b5b3eca70d84
Daniel Hirsmuller-Counts <linuxdan@gmail.com>
2010-05-16 22:04:38 -0700

    Replaced some tabs with spaces. Removed some more comments in erb files, which cause problems with Ruby 1.8.7. Receivings are now done for now. Two more models to go.

--- config/environment.rb
+++ config/environment.rb
@@ -19,12 +19,14 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
-  config.gem "rspec", :lib => false, :version => ">= 1.2.0" 
-  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0" 
-  config.gem "rack", :lib => false, :version => ">= 1.0.0"
   config.gem "authlogic"
   config.gem 'hoptoad_notifier'
+  config.gem "rack", :lib => false, :version => ">= 1.0.0"
+  config.gem "rspec", :lib => false, :version => ">= 1.2.0" 
+  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0" 
+  config.gem "shopify_api", :version => ">= 1.0.2"
+  config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
+  
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- public/javascripts/variant_select.js
+++ public/javascripts/variant_select.js
@@ -2,34 +2,34 @@ $(document).ready(function() {
   /* Populate product variant checkboxes. */
   /* Now handled by observe_form
   $('#vendors').click(function() {
-		var vendor = $(this).val();
-		$('#productVariants').empty();
-		$('#productVariants').load("/ajax/vendor_products", {vendor_name: vendor}, null, stop_spinner($('#productVariants')));
-		start_spinner($('#productVariants'));
-	})
-	*/
+    var vendor = $(this).val();
+    $('#productVariants').empty();
+    $('#productVariants').load("/ajax/vendor_products", {vendor_name: vendor}, null, stop_spinner($('#productVariants')));
+    start_spinner($('#productVariants'));
+  })
+  */
 })
 
 function new_total(item)
 {
   var grandparent = $(item).parent().parent();
-	var new_count = Number($(item).val());
-	var old_count = Number(grandparent.children('td.oldCount').text());
-	var reason = $("#stock_adjustment_reason").val();
-	switch(reason)
-	{
-	  case "Defective":
-	  case "Donation":
-	    var total = old_count - new_count;
-	    break;
-	  default:
-	    var total = old_count + new_count;
-	    break;
-	}
-	if(!isNaN(total)){
-	  grandparent.children('td.newTotal').empty();
-	  grandparent.children('td.newTotal').text(total);
-	}
+  var new_count = Number($(item).val());
+  var old_count = Number(grandparent.children('td.oldCount').text());
+  var reason = $("#stock_adjustment_reason").val();
+  switch(reason)
+  {
+    case "Defective":
+    case "Donation":
+      var total = old_count - new_count;
+      break;
+    default:
+      var total = old_count + new_count;
+      break;
+  }
+  if(!isNaN(total)){
+    grandparent.children('td.newTotal').empty();
+    grandparent.children('td.newTotal').text(total);
+  }
 }
 
 function start_spinner(element)
