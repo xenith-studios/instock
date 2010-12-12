0f5b54da3b8d42676acefc922cb06f828b9045f9
Justin Rocha <justin@bazaarconsulting.com>
2010-12-12 14:11:08 -0800

    Remove shopify.yml again. Apparently I was passing the api key and secret in the login_controller. Update it. Figure it out later.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,13 +4,13 @@ class LoginController < ApplicationController
   end
 
   def authenticate
-    ShopifyAPI::Session.setup({:api_key => "b2da14e1291682a9dad049d4970f5541", :secret => "df6c9f0d84c9b7a65d92ec4132f7d877"})
+    ShopifyAPI::Session.setup({:api_key => "9b8ca7d587384d00267255bfd98cfe65", :secret => "4bb0add4f84f4a0181ed0e6d9ca048c5"})
     redirect_to ShopifyAPI::Session.new(params[:shop].chomp('/')).create_permission_url
   end
 
   # Shopify redirects the logged-in user back to this action along with
   # the authorization token t.
-  # 
+  #
   # This token is later combined with the developer's shared secret to form
   # the password used to call API methods.
   def finalize
@@ -18,19 +18,19 @@ class LoginController < ApplicationController
     if shopify_session.valid?
       session[:shopify] = shopify_session
       ActiveResource::Base.site = shopify_session.site
-      
+
       redirect_to :controller => 'client_shop_management', :action => 'record_shop'
-      
+
     else
       flash[:error] = "Could not log in to Shopify store."
       redirect_to :action => 'index'
     end
   end
-  
+
   def logout
     session[:shopify] = nil
     flash[:notice] = "Successfully logged out."
-    
+
     redirect_to :action => 'index'
   end
-end 
+end
