bf9de25e172e1ff4592ae741aa014357c5e7a2b3
Justin Rocha <justin@bazaarconsulting.com>
2011-06-01 18:31:07 -0700

    Update to latest version of hoptoad_notifier, shopify_api, and rails 2.3.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -20,11 +20,11 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
-  config.gem 'hoptoad_notifier', :version => "= 2.3.10"
+  config.gem 'hoptoad_notifier', :version => "= 2.4.11"
   config.gem "rack", :lib => false, :version => "= 1.1.0"
-  config.gem "rspec", :lib => false, :version => ">= 1.2.0" 
-  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0" 
-  config.gem "shopify_api", :version => "= 1.0.6"
+  config.gem "rspec", :lib => false, :version => ">= 1.2.0"
+  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
+  config.gem "shopify_api", :version => "= 1.2.2"
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
 
 
