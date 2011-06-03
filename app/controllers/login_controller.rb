c6864a79a7d29d4a1b51e579c3210431686238f3
Justin Rocha <justin@bazaarconsulting.com>
2011-06-03 14:42:33 -0700

    Move ShopifyAPI::Session.setup to the application_controller.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,7 +4,6 @@ class LoginController < ApplicationController
   end
 
   def authenticate
-    ShopifyAPI::Session.setup({:api_key => "9b8ca7d587384d00267255bfd98cfe65", :secret => "4bb0add4f84f4a0181ed0e6d9ca048c5"})
     redirect_to ShopifyAPI::Session.new(params[:shop].chomp('/')).create_permission_url
   end
 
@@ -14,7 +13,6 @@ class LoginController < ApplicationController
   # This token is later combined with the developer's shared secret to form
   # the password used to call API methods.
   def finalize
-    ShopifyAPI::Session.setup({:api_key => "9b8ca7d587384d00267255bfd98cfe65", :secret => "4bb0add4f84f4a0181ed0e6d9ca048c5"})
     shopify_session = ShopifyAPI::Session.new(params[:shop], params[:t], params)
     if shopify_session.valid?
       session[:shopify] = shopify_session
